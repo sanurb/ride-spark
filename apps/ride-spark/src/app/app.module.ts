fa4a66366d508cb5de70d1d1d5fa603014ad6b2b
David Urbano <david.urbano@uao.edu.co>
2024-02-29 09:55:29 -0500

    feat: add NestJS Config and TypeORM, Update Dependencies

--- apps/ride-spark/src/app/app.module.ts
+++ apps/ride-spark/src/app/app.module.ts
@@ -1,12 +1,37 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DriverModule } from '@ride-spark/driver';
 import { CorrelationIdMiddleware } from '@ride-spark/infra-nest-server';
+import { ProblemModule } from '@ride-spark/nest/problem';
+import { RiderModule } from '@ride-spark/rider';
+import { EnvConfiguration } from '../config/env.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProblemModule } from '@ride-spark/nest/problem';
 
 @Module({
   imports: [
-    ProblemModule
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'production'}`,
+      load: [ EnvConfiguration ],
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+
+    ProblemModule,
+    DriverModule,
+    RiderModule
   ],
   controllers: [AppController],
   providers: [AppService],
